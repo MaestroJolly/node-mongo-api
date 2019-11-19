bd979c412b32b0b82e6ebd903b0cb29877549a3a
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2019-11-19 23:22:07 +0100

    Update: MongoDB connection set up

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const express = require('express');
 const mongo = require('mongodb').MongoClient;
 const PORT = process.env.PORT || '3000';
 const app = express();
+const url = process.env.MONGOURL;
+
+const dbName = process.env.DATABASENAME;
+
+const mongoObject = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
 
 app.use(bodyParser.json());
 
@@ -16,6 +24,38 @@ app.get('*', (req, res)=>{
         greetings: 'Hello World'
     });
 });
+
+mongo.connect(url, mongoObject, (err, client) => {
+
+  if (err) {
+    console.error(err)
+    return
+  }
+  
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+
+  const collection = db.collection('users');
+
+//   collection.insertOne({name: 'john doe'})
+//   .then(item => {
+//     console.log(item)
+//   })
+//   .catch(err => {
+//   console.error(err)
+//   })
+
+  collection.findOne({name: 'john doe'})
+  .then(item => {
+    console.log(item)
+  })
+  .catch(err => {
+  console.error(err)
+  })
+ 
+  client.close();
+})
 
 app.listen(PORT, ()=>{
     console.log(`Application is listening to PORT: ${PORT}`);
